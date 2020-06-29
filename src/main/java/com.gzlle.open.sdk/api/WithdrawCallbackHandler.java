d0eda7d177a50142301d5b99a7b03b9759b4ce99
15580799040 <15580799040@163.com>
2020-06-29 17:55:01 +0800

    首次修改

--- src/main/java/com.gzlle.open.sdk/api/WithdrawCallbackHandler.java
+++ src/main/java/com.gzlle.open.sdk/api/WithdrawCallbackHandler.java
@@ -1,6 +1,6 @@
 package com.gzlle.open.sdk.api;
 
-import com.gzlle.open.sdk.dto.ContractCallbackDTO;
+
 import com.gzlle.open.sdk.dto.withdraw.WithdrawCallbackDTO;
 
 public interface WithdrawCallbackHandler {
